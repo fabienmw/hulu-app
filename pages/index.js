8ec405fd9f5c4063f3264ac27b13b484e27e1893
Fabien <fabien.mwamba@gmail.com>
2022-01-17 23:43:10 +0200

    Styling thumbanil

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import requests from '../utils/requests'
 import Header from './components/Header'
 import Nav from './components/Nav'
+import Results from './components/Results'
 
-export default function Home() {
+export default function Home({ results }) {
+
+  // console.log(results);
+
   return (
     <div>
       <Head>
@@ -13,9 +18,26 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* { navbard} */}
+
       <Nav />
-      {/* {results} */}
+      
+      <Results results={results}/>
+
     </div>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`)
+  .then(res => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
+
 }
