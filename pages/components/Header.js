4b02072b68f1c012964bc393a0091f4b621bb966
Fabien <fabien.mwamba@gmail.com>
2022-01-17 15:23:02 +0200

    Added header component

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,9 +1,21 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from '@heroicons/react/outline'
+
 import Image from 'next/image'
+import HeaderItem from './HeaderItem'
 
 function Header() {
     return (
         <header className="">
-            <h1>Header</h1>
+            <div>
+                <HeaderItem title='HOME' Icon={HomeIcon}/>
+            </div>
             <Image
             className='object-contain'
             src="https://links.papareact.com/ua6" width={200} height={100}/>
